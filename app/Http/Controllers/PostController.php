a5e7a87631ceb3d0cf42336f9e1bf8eb18a8f91f
anas nashat <aanasht@gmail.com>
2025-03-08 15:05:54 +0200

    add the routes for the resource controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -73,7 +73,7 @@ class PostController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Post $post)
+    public function destroy(string $post)
     {
         //
     }
