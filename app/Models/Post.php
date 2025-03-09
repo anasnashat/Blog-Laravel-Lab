baf8b067eb8a1df87a2fbf26f9f6514bfbfe9de2
anas nashat <aanasht@gmail.com>
2025-03-09 13:32:50 +0200

    implement soft deletes in Post model and comment out getRouteKey method

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,18 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
+    use softDeletes;
     protected $fillable = ['title', 'slug', 'description', 'user_id'];
 
-    public function getRouteKey()
-    {
-        return 'slug';
-    }
+//    public function getRouteKey()
+//    {
+//        return $this->slug;
+//    }
 
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
+
+
 }
