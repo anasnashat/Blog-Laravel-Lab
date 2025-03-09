65c5d46c6baa7c023afb33c49148800426951df2
anas nashat <aanasht@gmail.com>
2025-03-09 13:49:02 +0200

    add PostSeeder and enable HasFactory trait in Post model

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    protected $model = Post::class;
+    public function definition(): array
+    {
+        $title = $this->faker->sentence;
+
+        return [
+            'title' => $title,
+            'slug' => Str::slug($title),
+            'description' => $this->faker->paragraph,
+            'user_id' => 1,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+    }
+}
