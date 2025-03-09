06df1c608d606f0d9421b0cd3694a7b9793dfe6d
anas nashat <aanasht@gmail.com>
2025-03-09 09:43:12 +0200

    make the Post model and add the relation with the user

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
-    //
+    protected $fillable = ['title', 'slug', 'description', 'user_id'];
+
+    public function getRouteKey()
+    {
+        return 'slug';
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
