1f5736526e4b4fb4feef3e68c0388bf48d03f8ea
anas nashat <aanasht@gmail.com>
2025-03-09 13:32:17 +0200

    list the data from the database and add the pagination to the poroject and redirect to the dashboard and the soft delete

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -21,14 +21,21 @@
                 <tbody class="divide-y divide-gray-200">
                 @foreach ($posts as $post)
                     <tr>
-                        <td class="px-4 py-2 font-medium whitespace-nowrap text-gray-900">{{ $post['id'] }}</td>
+                        <td class="px-4 py-2 font-medium whitespace-nowrap text-gray-900">{{ $post->id }}</td>
                         <td class="px-4 py-2 whitespace-nowrap text-gray-700">{{$post['title']}}</td>
-                        <td class="px-4 py-2 whitespace-nowrap text-gray-700">{{ $post['posted_by'] }}</td>
-                        <td class="px-4 py-2 whitespace-nowrap text-gray-700">{{ $post['created_at'] }}</td>
+                        <td class="px-4 py-2 whitespace-nowrap text-gray-700">{{ $post->user->name}}</td>
+                        <td class="px-4 py-2 whitespace-nowrap text-gray-700">{{ $post->created_at->format("Y-m-d") }}</td>
                         <td class="px-4 py-2 whitespace-nowrap text-gray-700 space-x-2">
-                            <a href="{{ route('posts.show', $post['id']) }}" class="inline-block px-4 py-1 text-xs font-medium text-white bg-blue-400 rounded hover:bg-blue-500">View</a>
+                            <a href="{{ route('posts.show', $post) }}" class="inline-block px-4 py-1 text-xs font-medium text-white bg-blue-400 rounded hover:bg-blue-500">View</a>
+                            @if(auth()->id() == $post->user->id)
                             <a href="{{ route("posts.edit", $post['id']) }}" class="inline-block px-4 py-1 text-xs font-medium text-white bg-blue-600 rounded hover:bg-blue-700">Edit</a>
-                            <a href="#" class="inline-block px-4 py-1 text-xs font-medium text-white bg-red-600 rounded hover:bg-red-700">Delete</a>
+                            <form action="{{ route('posts.destroy', $post) }}" method="POST" class="inline-block" onsubmit="return confirm('Are you sure to delete post {{$post->name}}?')">
+                                @csrf
+                                @method("DELETE")
+                                <button type="submit" class="inline-block px-4 py-1 text-xs font-medium text-white bg-red-600 rounded hover:bg-red-700">Delete</button>
+{{--                            <a href="{{ route('posts.destroy', $post) }}" class="inline-block px-4 py-1 text-xs font-medium text-white bg-red-600 rounded hover:bg-red-700">Delete</a>--}}
+                            </form>
+                            @endif
                         </td>
                     </tr>
                 @endforeach
@@ -37,35 +44,9 @@
         </div>
 
         <!-- Pagination -->
-        <div class="rounded-b-lg border-t border-gray-200 px-4 py-2">
-            <ol class="flex justify-end gap-1 text-xs font-medium">
-                <li>
-                    <a href="#" class="inline-flex size-8 items-center justify-center rounded-sm border border-gray-100 bg-white text-gray-900">
-                        <span class="sr-only">Prev Page</span>
-                        <svg xmlns="http://www.w3.org/2000/svg" class="size-3" viewBox="0 0 20 20" fill="currentColor">
-                            <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
-                        </svg>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" class="block size-8 rounded-sm border border-gray-100 bg-white text-center leading-8 text-gray-900">1</a>
-                </li>
-                <li class="block size-8 rounded-sm border-blue-600 bg-blue-600 text-center leading-8 text-white">2</li>
-                <li>
-                    <a href="#" class="block size-8 rounded-sm border border-gray-100 bg-white text-center leading-8 text-gray-900">3</a>
-                </li>
-                <li>
-                    <a href="#" class="block size-8 rounded-sm border border-gray-100 bg-white text-center leading-8 text-gray-900">4</a>
-                </li>
-                <li>
-                    <a href="#" class="inline-flex size-8 items-center justify-center rounded-sm border border-gray-100 bg-white text-gray-900">
-                        <span class="sr-only">Next Page</span>
-                        <svg xmlns="http://www.w3.org/2000/svg" class="size-3" viewBox="0 0 20 20" fill="currentColor">
-                            <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
-                        </svg>
-                    </a>
-                </li>
-            </ol>
+        <div class="rounded-b-lg border-t border-gray-200 px-4 py-2 bg-white text-black">
+            {{ $posts->links() }}
         </div>
+
     </div>
 </x-layout>
